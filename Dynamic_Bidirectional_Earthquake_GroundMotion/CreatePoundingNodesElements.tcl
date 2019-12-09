d5e0427e22409bb036f5bfee343b948bdd58cf72
sadilak <56599783+sadilak85@users.noreply.github.com>
2019-12-10 00:04:09 +0100

    Add files via upload

--- Dynamic_Bidirectional_Earthquake_GroundMotion/CreatePoundingNodesElements.tcl
+++ Dynamic_Bidirectional_Earthquake_GroundMotion/CreatePoundingNodesElements.tcl
@@ -9,14 +9,14 @@
 #		Pounding Parameters:
 #
 set direction 1; # direction of normal of contact surface
-set initGap 0.5; # initial gap  5cm = 1.97 inch
-set frictionRatio 0.3; # friction ratio 
 set mu 0.4; # friction coefficient 
-set Kt 1.0e3; # penalty stiffness for tangential directions 		
-set Kn 1.0e3; # penalty stiffness for normal direction		 
-set Kn2 [expr $Kn * 0.1]; # penalty stiffness after yielding, based on Hertz impact model 
-set Delta_y 0.01; # yield displacement based on Hertz impact model 	  
+set Kt 1.0e5; # penalty stiffness for tangential directions 		
+set Kn 1.0e15; # penalty stiffness for normal direction		 
 set cohesion 1.0e5; # cohesion
+set Kn2 [expr $Kn * 0.1]; # penalty stiffness after yielding, based on Hertz impact model 
+set initGap 0.01; # initial gap  5cm = 1.97 inch
+set frictionRatio 0.3; # friction ratio 
+set Delta_y 0.01; # yield displacement based on Hertz impact model 
 #
 #
 set outFileforPoundingSTR CreatePoundingCommands.tcl
@@ -115,10 +115,10 @@ if [catch {open [lindex $PoundingInputFileList 0] r} inFileID] {
 						set eltID [lindex $tags 0]
 						set nodei [lindex $tags 1]
 						set nodej [lindex $tags 2]
-					#	set poundingstr "element zeroLengthImpact3D	"
-						set poundingstr "element zeroLengthContact3D "
-						#append poundingstr $eltID "	" $nodei "	" $nodej "	" $direction "	" $initGap "	" $frictionRatio "	" $Kt "	" $Kn "	" $Kn2 "	" $Delta_y "	" $cohesion
-						append poundingstr $eltID "	" $nodei "	" $nodej "	" $Kn "	" $Kt "	" $mu "	" $cohesion "	" $direction
+						set poundingstr "element zeroLengthImpact3D	"
+						append poundingstr $eltID "	" $nodei "	" $nodej "	" $direction "	" $initGap "	" $frictionRatio "	" $Kt "	" $Kn "	" $Kn2 "	" $Delta_y "	" $cohesion
+						#set poundingstr "element zeroLengthContact3D "
+						#append poundingstr $eltID "	" $nodei "	" $nodej "	" $Kn "	" $Kt "	" $mu "	" $cohesion "	" $direction
 						lappend poundingimpactList $poundingstr
 					}	
 					break
@@ -163,8 +163,8 @@ for {set i 0} {$i <= [expr [llength [lindex $poundingimpactList]]-1]} {incr i 1}
 	puts $outFileforPounding [lindex $poundingimpactList $i]
 }
 # springs with very low stiffness for convergance of Newton-Raphson method 
-set poundingstr "\nuniaxialMaterial Elastic 6 1.0e-3\n";	# mat 6
-append poundingstr "uniaxialMaterial Elastic 7 1.0e-3";	# mat 7
+set poundingstr "\nuniaxialMaterial Elastic 6 1.0e3\n";	# mat 6
+append poundingstr "uniaxialMaterial Elastic 7 1.0e3";	# mat 7
 puts $outFileforPounding $poundingstr
 for {set i 0} {$i <= [expr [llength [lindex $poundingelementList]]-1]} {incr i 1} {
 	puts $outFileforPounding [lindex $poundingelementList $i]
